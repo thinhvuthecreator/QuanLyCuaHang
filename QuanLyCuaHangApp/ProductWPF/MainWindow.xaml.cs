e337cfe9c5eec07a764c11666b30dc3f60333ba0
thinhvuthecreator <thinhvuh@gmail.com>
2022-12-12 11:07:45 +0700

    dang hoan thanh them hoadon

--- QuanLyCuaHangApp/ProductWPF/MainWindow.xaml.cs
+++ QuanLyCuaHangApp/ProductWPF/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 using Models;
+using System.Data;
+using System.Data.SqlClient;
 using SQL_Connection;
 
 namespace ProductWPF
@@ -27,21 +29,44 @@ namespace ProductWPF
         public MainWindow()
         {
             InitializeComponent();
+            loadForm();
         }
 
 
         #region methods
 
+        string returnMaLoaiSPcuaCombobox()
+        {
+            string LoaiSPcmb = loaiSPcombobox.SelectedItem.ToString();
+            string Querry = "SELECT MALOAISP FROM LOAISP WHERE TENLOAISP =N'" +LoaiSPcmb+ "'"; // Cau lenh lay ra maloaisp
+            DataTable data = SQL_Connect.Instance.ExecuteSQL(Querry); // tra ra maloaisp cua selected combobox
+            string ketQua = data.Rows[0][0].ToString();
+            return ketQua;
+        }
+
+        void loadForm()
+        {
+            DataTable LoaiSPSource = LoaiSanPham_DAL.loadDuLieuLOAISP();
+            foreach(DataRow data in LoaiSPSource.Rows) 
+            {
+                loaiSPcombobox.Items.Add(data[1]);
+            }
+
+        }
+          
+
         void ganGiaTriSP(SanPham sp)
         {
-            int maSP;
+            
+
             decimal giaSP;
             int soLuong;
-            sp.MaSP = int.TryParse(maSPTxtbox.Text,out maSP) == true ? maSP : -1;
+            
             sp.TenSP = tenSPTxtbox.Text;
             sp.GiaSP = decimal.TryParse(giaTxtbox.Text, out giaSP) == true ? giaSP : -1;
             sp.NgThemSp = DateTime.Parse(ngThemspDatePicker.Text);
             sp.SoLuongSP = int.TryParse(soluongTxtbox.Text, out soLuong) == true ? soLuong : -1;
+            sp.MaLoaiSP = int.Parse(returnMaLoaiSPcuaCombobox());
 
         }
 
@@ -60,7 +85,7 @@ namespace ProductWPF
                 System.IO.File.Copy(sourceAnh, sourceAnhApp, true);
             }
 
-
+            
 
         }
         private void huyBtn_Click(object sender, RoutedEventArgs e)
--- QuanLyCuaHangApp/addHoaDonWPF/MainWindow.xaml.cs
+++ QuanLyCuaHangApp/addHoaDonWPF/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data;
+using System.Data.SqlClient;
+using SQL_Connection;
 namespace addHoaDonWPF
 {
     /// <summary>
@@ -23,6 +25,53 @@ namespace addHoaDonWPF
         public MainWindow()
         {
             InitializeComponent();
+
+
+            loadNVcmb();
+            loadKHcmb();
+            loadSPcmb();
+
+        }
+
+        void loadNVcmb()
+        {
+            string laytenNVQuerry = "SELECT TENNV FROM NHANVIEN";
+            DataTable dataNhanvien = SQL_Connect.Instance.ExecuteSQL(laytenNVQuerry);
+
+            foreach (DataRow data in dataNhanvien.Rows)
+            {
+                nhanVienCombobox.Items.Add(data[0]);
+            }
+        }
+
+        void  loadKHcmb()
+        {
+            string laytenKHQuerry = "SELECT TENKH FROM KHACHHANG";
+            DataTable dataKH = SQL_Connect.Instance.ExecuteSQL(laytenKHQuerry);
+
+            foreach (DataRow data in dataKH.Rows)
+            {
+                khachhangCombobox.Items.Add(data[0]);
+            }
+        }
+
+        void loadSPcmb()
+        {
+            string laytenSPQuerry = "SELECT TENSP FROM SANPHAM";
+            DataTable dataSP = SQL_Connect.Instance.ExecuteSQL(laytenSPQuerry);
+
+            foreach (DataRow data in dataSP.Rows)
+            {
+                sanPhamCombobox.Items.Add(data[0]);
+            }
+        }
+
+        public class danhSachSanPham
+        {
+            public string tenSP;
+            public int soLuong;
+            public int gia;
+            
         }
     }
 }
