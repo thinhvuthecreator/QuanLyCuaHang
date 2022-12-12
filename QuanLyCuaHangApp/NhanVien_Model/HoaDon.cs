202849d3e0d7133e86d9a9cedef184f339d2bcfa
thinhvuthecreator <thinhvuh@gmail.com>
2022-12-12 22:26:14 +0700

    50% them hoa Don

---
+++ QuanLyCuaHangApp/NhanVien_Model/HoaDon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class HoaDon
+    {
+        private DateTime ngHoaDon;
+        private int triGia, maKH, maNV;
+
+        public DateTime NgHoaDon { get => ngHoaDon; set => ngHoaDon = value; }
+        public int TriGia { get => triGia; set => triGia = value; }
+        public int MaKH { get => maKH; set => maKH = value; }
+        public int MaNV { get => maNV; set => maNV = value; }
+    }
+}
--- QuanLyCuaHangApp/addHoaDonWPF/MainWindow.xaml.cs
+++ QuanLyCuaHangApp/addHoaDonWPF/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Data;
 using System.Data.SqlClient;
 using SQL_Connection;
+using Models;
 namespace addHoaDonWPF
 {
     /// <summary>
@@ -22,6 +23,8 @@ namespace addHoaDonWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int soLuong = 0;
+        List<danhSachSanPham> dsSP = new List<danhSachSanPham>();
         public MainWindow()
         {
             InitializeComponent();
@@ -32,46 +35,185 @@ namespace addHoaDonWPF
             loadSPcmb();
 
         }
+        #region object
+        public class danhSachSanPham
+        {
+            public string tenSP { get; set; }
+            public int SoLuong { get; set; }
+            public decimal gia { get; set; }
+
+        }
+        public class NhanVienAo
+        {
+            public int maNV { get; set; }
+            public string tenNV { get; set; }
+            public NhanVienAo()
+            {
+
+            }
+            public NhanVienAo(int ma, string ten)
+            {
+                maNV = ma;
+                tenNV = ten;
+            }
+        }
+
+        public class KhachHangAo
+        {
+            public int maKH { get; set; }
+            public string tenKH { get; set; }
+            KhachHangAo()
+            {
+
+            }
+            public KhachHangAo(int ma, string ten)
+            {
+                maKH = ma;
+                tenKH = ten;
+            }
+        }
+
+        public class SanPhamAo
+        {
+            public int maSP { get; set; }
+            public string tenSP { get; set; }
+            SanPhamAo()
+            {
+
+            }
+            public SanPhamAo(int ma, string ten)
+            {
+                maSP = ma;
+                tenSP = ten;
+            }
+        }
+        #endregion
+
+        #region methods
 
         void loadNVcmb()
         {
-            string laytenNVQuerry = "SELECT TENNV FROM NHANVIEN";
+            List<NhanVienAo> listNV = new List<NhanVienAo>();
+            string laytenNVQuerry = "SELECT MANV,TENNV FROM NHANVIEN";
             DataTable dataNhanvien = SQL_Connect.Instance.ExecuteSQL(laytenNVQuerry);
 
             foreach (DataRow data in dataNhanvien.Rows)
             {
-                nhanVienCombobox.Items.Add(data[0]);
+                
+                NhanVienAo nvAo = new NhanVienAo(int.Parse(data[0].ToString()), data[1].ToString());
+                listNV.Add(nvAo);
             }
+
+            nhanVienCombobox.ItemsSource = listNV;
+            nhanVienCombobox.DisplayMemberPath = "tenNV";
+            nhanVienCombobox.SelectedValuePath = "maNV";
         }
 
         void  loadKHcmb()
         {
-            string laytenKHQuerry = "SELECT TENKH FROM KHACHHANG";
+            List<KhachHangAo> listKH = new List<KhachHangAo>();
+            string laytenKHQuerry = "SELECT MAKH,TENKH FROM KHACHHANG";
             DataTable dataKH = SQL_Connect.Instance.ExecuteSQL(laytenKHQuerry);
 
             foreach (DataRow data in dataKH.Rows)
             {
-                khachhangCombobox.Items.Add(data[0]);
+
+                KhachHangAo khAo = new KhachHangAo(int.Parse(data[0].ToString()), data[1].ToString());
+                listKH.Add(khAo);
             }
+
+            khachhangCombobox.ItemsSource = listKH;
+            khachhangCombobox.DisplayMemberPath = "tenKH";
+            khachhangCombobox.SelectedValuePath = "maKH";
         }
 
         void loadSPcmb()
         {
-            string laytenSPQuerry = "SELECT TENSP FROM SANPHAM";
+            List<SanPhamAo> listSP = new List<SanPhamAo>();
+            string laytenSPQuerry = "SELECT MASP,TENSP FROM SANPHAM";
             DataTable dataSP = SQL_Connect.Instance.ExecuteSQL(laytenSPQuerry);
 
             foreach (DataRow data in dataSP.Rows)
             {
-                sanPhamCombobox.Items.Add(data[0]);
+
+                SanPhamAo spAo = new SanPhamAo(int.Parse(data[0].ToString()), data[1].ToString());
+                listSP.Add(spAo);
+            }
+
+            sanPhamCombobox.ItemsSource = listSP;
+            sanPhamCombobox.DisplayMemberPath = "tenSP";
+            sanPhamCombobox.SelectedValuePath = "maSP";
+        }
+
+        int layMaNV()
+        {
+            return int.Parse(nhanVienCombobox.SelectedValue.ToString());
+        }
+
+        int layMaKH()
+        {
+            return int.Parse(khachhangCombobox.SelectedValue.ToString());
+        }
+        #endregion
+        void themSPListView()
+        {
+            decimal Gia = 0;
+
+            soLuong++;
+
+            DataTable data = SQL_Connect.Instance.ExecuteSQL("SELECT GIA FROM SANPHAM WHERE MASP = " + sanPhamCombobox.SelectedValue);
+
+            foreach (DataRow dt in data.Rows)
+            {
+                Gia = decimal.Parse(dt[0].ToString());
+            }
+
+            danhSachSanPham ds = new danhSachSanPham();
+            ds.tenSP = sanPhamCombobox.Text;
+            ds.gia = Gia;
+            ds.SoLuong = soLuong;
+            dsSP.Add(ds);                 // dsSP là List<danhsachsanpham>
+            dsSPListview.Items.Add(ds);
+        }
+
+        #region events
+       
+
+       
+
+        private void addHDBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HoaDon hd = new HoaDon();
+            hd.TriGia = int.Parse(tongtriGiaTextbox.Text);
+            hd.MaKH = layMaKH();
+            hd.MaNV = layMaNV();
+        }
+
+        #endregion 
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            themSPListView();
+           
+        }
+
+        private void reduceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (soLuong == 0)
+            {
+                MessageBox.Show("Không có sản phẩm này trong giỏ hàng !");
+            }
+            else
+            {
+                soLuong--;
+                MessageBox.Show(soLuong.ToString());
             }
         }
 
-        public class danhSachSanPham
+        private void huyThemHDBtn_Click(object sender, RoutedEventArgs e)
         {
-            public string tenSP;
-            public int soLuong;
-            public int gia;
-            
+            this.Close();
         }
     }
 }
