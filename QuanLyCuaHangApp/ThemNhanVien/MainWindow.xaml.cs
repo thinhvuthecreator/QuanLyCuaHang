26d64ce20663ebb228cac360afb422557d16a965
thinhvuthecreator <thinhvuh@gmail.com>
2023-01-16 13:28:12 +0700

    xong form them nhan vien moi

--- QuanLyCuaHangApp/ThemNhanVien/MainWindow.xaml.cs
+++ QuanLyCuaHangApp/ThemNhanVien/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.Win32;
+using SQL_Connection;
+using Models;
 namespace ThemNhanVien
 {
     /// <summary>
@@ -23,6 +25,82 @@ namespace ThemNhanVien
         public MainWindow()
         {
             InitializeComponent();
+            loadImages();
+        }
+
+        #region methods
+
+        void ganGiaTriNhanVien(NhanVien nv)
+        {
+            #region xuLyChuoiCombobox
+            string[] chuoi = genderCombobox.SelectedValue.ToString().Split();
+            string gioiTinh = chuoi[1];
+            #endregion
+
+            int sdt, luong;
+
+            nv.TenNV = tenNVTxtbox.Text;
+            nv.GioiTinh = gioiTinh;
+            nv.SdtNV = int.TryParse(sdtNVTxtbox.Text, out sdt) == true ? sdt : 0;
+            nv.NgSinhNV = DateTime.Parse(ngsinhNVDatePicker.Text);
+            nv.LuongNV = int.TryParse(luongTxtbox.Text, out luong) == true ? luong : 0;
+            nv.FileAnh = loadimageImage.Source.ToString();
+        }
+
+        void loadImages()
+        {
+            string resourceImage1 = System.IO.Path.GetFullPath("imageDefault.jpg");
+            BitmapImage logoStaff = new BitmapImage();
+            logoStaff.BeginInit();
+            logoStaff.UriSource = new Uri(resourceImage1);
+            logoStaff.EndInit();
+            loadimageImage.Source = logoStaff;
+
+        }
+
+
+
+        #endregion
+
+        private void addImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog loadImage = new OpenFileDialog();
+            if (loadImage.ShowDialog() == true)
+            {
+
+
+                string sourceAnh = loadImage.FileName;
+                string sourceAnhApp = System.IO.Path.GetFullPath(System.IO.Path.GetFileName(loadImage.FileName));
+                System.IO.File.Copy(sourceAnh, sourceAnhApp, true);
+                Uri fileUri = new Uri(sourceAnhApp);
+                loadimageImage.Source = new BitmapImage(fileUri);
+
+
+            }
+
+        }
+
+        private void huyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void themNVBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NhanVien nv = new NhanVien();
+            ganGiaTriNhanVien(nv);
+            if (NhanVien_DAL.themNhanVien(nv))
+            {
+                MessageBox.Show("thêm thành công !");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("thêm thất bại !");
+
+            }
+
+
         }
     }
 }
