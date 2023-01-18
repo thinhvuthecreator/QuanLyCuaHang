5c9bf8c0ae72f497e53ab25763c423eed1450446
thinhvuthecreator <thinhvuh@gmail.com>
2023-01-18 18:59:06 +0700

    90% xong phan mem, sualoi lat vat va giao dien tron

--- QuanLyCuaHangApp/ThongKe/ViewModel.cs
+++ QuanLyCuaHangApp/ThongKe/ViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using SQL_Connection;
 namespace ThongKe
 {
     public class ViewModel
@@ -15,11 +16,27 @@ namespace ThongKe
         {
             data = new List<Sale>()
             {
-                new Sale {doanhThu = 120000,thangNay = "tháng trước" },
-                new Sale {doanhThu = 150000,thangNay = "tháng này"}
+                new Sale {doanhThu = doanhThuThang() ,thangNay = "tháng này"}
+
+               
 
             };
+
         }
+
+        decimal doanhThuThang()
+        {
+            DataTable doanhThu = SQL_Connect.Instance.ExecuteSQL("SELECT SUM(TRIGIASAUKM) FROM HOADON WHERE MONTH(NGHD) = MONTH(GETDATE())");
+            if (doanhThu.Rows.Count > 0)
+            {
+                return decimal.Parse(doanhThu.Rows[0][0].ToString());
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
 
     }
 }
