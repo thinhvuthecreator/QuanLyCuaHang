9d37a5cff46c0a20f78c7347ab8987614a934a8a
thinhvuthecreator <thinhvuh@gmail.com>
2023-01-18 12:33:16 +0700

    chi can lam bieu do thong ke nua la tam xong

--- QuanLyCuaHangApp/ThongKe/MainWindow.xaml.cs
+++ QuanLyCuaHangApp/ThongKe/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SQL_Connection;
+using Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ThongKe
 {
@@ -23,6 +27,81 @@ namespace ThongKe
         public MainWindow()
         {
             InitializeComponent();
+            loadListThongKeSP();
+            loadThongTinDongTien();
         }
+
+        #region objects
+        public class sanPham
+        {
+            public int maSP { get; set; }
+            public string tenSP { get; set; }
+            public int soluongNhap { get; set; }
+            public int soluongBan { get; set; }
+            public sanPham()
+            {
+                soluongBan = 0;
+            }
+        }
+        #endregion
+
+        #region methods
+        string luongNV()
+        {
+            DataTable data = SQL_Connect.Instance.ExecuteSQL("SELECT SUM(LUONG) FROM NHANVIEN");
+            return data.Rows[0][0].ToString();
+        }
+        string tienNhapHang()
+        {
+            DataTable data = SQL_Connect.Instance.ExecuteSQL("SELECT SUM(GIA) FROM SANPHAM");
+            return data.Rows[0][0].ToString();
+        }
+        string doanhThu()
+        {
+            DataTable data = SQL_Connect.Instance.ExecuteSQL("SELECT SUM(TRIGIASAUKM) FROM HOADON");
+            return data.Rows[0][0].ToString();
+        }
+        string lai()
+        {
+            return (decimal.Parse(doanhThu()) - (decimal.Parse(luongNV()) + decimal.Parse(tienNhapHang()))).ToString();
+        }
+
+        void loadThongTinDongTien()
+        {
+            luongNVTextblock.Text = luongNV() + " VND";
+            tienHangTextblock.Text = tienNhapHang() + " VND";
+            doanhthuTextblock.Text = doanhThu() + " VND";
+            laiTextblock.Text = lai() + " VND";
+        }
+        void loadListThongKeSP()
+        {
+            List<sanPham> listSP = new List<sanPham>();
+            
+            DataTable dataTatCaSP = SQL_Connect.Instance.ExecuteSQL("SELECT MASP,TENSP,SOLUONGSP FROM SANPHAM");
+            foreach(DataRow row in dataTatCaSP.Rows)
+            {
+                listSP.Add(new sanPham { maSP = int.Parse(row[0].ToString()),tenSP = row[1].ToString(), soluongNhap = int.Parse(row[2].ToString())});
+            }
+
+            DataTable dataSP_in_CTHD = SQL_Connect.Instance.ExecuteSQL("SELECT MASP,SUM(SOLUONG) as BANDUOC FROM CTHD GROUP BY MASP");
+            foreach (sanPham sp in listSP)
+            {
+                foreach (DataRow row in dataSP_in_CTHD.Rows)
+                {
+                    if(sp.maSP == int.Parse(row[0].ToString()))
+                    {
+                        sp.soluongBan = int.Parse(row[1].ToString());
+                    }
+                }
+            }
+            listSP_Listview.ItemsSource = listSP;
+        }
+
+        #endregion
+
     }
+
+
+
+
 }
